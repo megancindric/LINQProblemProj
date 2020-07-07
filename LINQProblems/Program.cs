130e940395fc277bb6f5d7f4c230fafccd4fa769
megancindric <megancindric@gmail.com>
2020-07-07 12:29:01 -0500

    Added code for question 2 to program class

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace LINQProblems
@@ -9,14 +10,19 @@ namespace LINQProblems
         static void Main(string[] args)
         {
 
+         /* Question 2
+          
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            var distinctWords = names.Distinct();
+            foreach (var word in distinctWords)
+            {
+                Console.WriteLine(word);
+            }
 
+         */
 
-
-
-
-
-
-            /* QUESTION 1
+         /* QUESTION 1
+          
           List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
 
             var wordsWithTh = words.Where(w => w.Contains("th"));
@@ -24,7 +30,8 @@ namespace LINQProblems
                 {
                     Console.WriteLine(w);
                 }
-            */
+            
+         */
 
             Console.ReadLine();
         }
